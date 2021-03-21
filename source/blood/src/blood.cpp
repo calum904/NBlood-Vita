9eec893dab2682a476167475d2af7627c7912e0a
Calum Judd Anderson <calum122@live.co.uk>
2021-03-21 18:36:24 +0000

    Set 'bNoDemo' to true as a work around to get into 'main' loop

--- source/blood/src/blood.cpp
+++ source/blood/src/blood.cpp
@@ -93,7 +93,7 @@ unsigned int nMaxAlloc = 0x4000000;
 
 bool bCustomName = false;
 char bAddUserMap = false;
-bool bNoDemo = false;
+bool bNoDemo = true;
 bool bQuickStart = true;
 bool bNoAutoLoad = false;
 
